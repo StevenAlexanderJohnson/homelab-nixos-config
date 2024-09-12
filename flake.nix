dde6824f92fdc406b45a077e71656d469562bde6
Steven Johnson <steveajohn1@gmail.com>
2024-09-12 09:12:14 -0400

    moving hardware configuration to hardware folder

--- flake.nix
+++ flake.nix
@@ -5,26 +5,28 @@
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
 	};
 
-	outputs = { self, nixpkgs, ... }@inputs: let
-	nodes = [
-		"node1"
-		"node2"
-		"node3"
-	];
+	outputs = { self, nixpkgs, ... }@inputs:
+  let
+    nodes = [
+      "node1"
+      "node2"
+      "node3"
+    ];
+		importHardware = node: ./. + "/hardware/${node}.nix";
 	in {
 	    nixosConfigurations = builtins.listToAttrs (map (name: {
-			name = name;
+      inherit name;
 			value = nixpkgs.lib.nixosSystem {
 				specialArgs = {
-				meta = { hostname = name; };
+					meta = { hostname = name; };
+				};
+				system = "x86_64-linux";
+				modules = [
+					# Modules
+					(importHardware name)
+					./configuration.nix
+				];
 			};
-			system = "x86_64-linux";
-			modules = [
-				# Modules
-				./hardware-configuration.nix
-				./configuration.nix
-			];
-		};
-	    }) nodes);
-	  };
+		}) nodes);
+	};
 }
