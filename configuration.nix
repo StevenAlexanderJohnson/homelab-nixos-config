0f8f6a51b4e54fd3b15f0968b0ee44ce1739f1ae
Steven Johnson <steveajohn1@gmail.com>
2024-09-25 20:00:30 -0400

    adding neofetch

--- configuration.nix
+++ configuration.nix
@@ -104,6 +104,7 @@ in
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neofetch
     wget
     git
     k3s
